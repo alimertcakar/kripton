d58bc20b4210667c423a041a09d9382bd4abf975
Ali Mert <mail@alimert.net>
2020-06-12 18:06:53 +0300

    switch to caesar class

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-function shifter(char, shift) {
-    return char.charCodeAt() + shift;
-}
-var caesarShift = function (str, amount) {
-    return String.fromCharCode(...str.split("").map((c, index) => shifter(c, amount[index % amount.length])));
-};
 class caesar {
+    static shifter(char, shift) {
+        return char.charCodeAt() + shift;
+    }
     static encrypt(str, amount) {
         return String.fromCharCode(...str.split("").map((c, index) => shifter(c, amount[index % amount.length])));
     }
