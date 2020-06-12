8514a87802c5adf29c47de59571e0de477ade1bc
Ali Mert <mail@alimert.net>
2020-06-12 17:45:01 +0300

    kripton project finish

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ function shifter(char, shift) {
     return char.charCodeAt() + shift;
 }
 var caesarShift = function (str, amount) {
-    return String.fromCharCode(...str.split("").map(c => shifter(c, amount)));
+    return String.fromCharCode(...str.split("").map((c, index) => shifter(c, amount[index % amount.length])));
 };
 
 
+let text = "alimert";
+let sifreli = caesarShift(text, [1, 4, 5, 3])
+let sifresiz = caesarShift(sifreli, [-1, -4, -5, -3])
+
+console.log(sifreli, sifresiz);
