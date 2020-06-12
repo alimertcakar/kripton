9e7ae55be7b83d3a38929bb90481ae013ab5265c
Ali Mert <mail@alimert.net>
2020-06-12 18:06:25 +0300

    switch to class usage

--- index.js
+++ index.js
@@ -4,10 +4,20 @@ function shifter(char, shift) {
 var caesarShift = function (str, amount) {
     return String.fromCharCode(...str.split("").map((c, index) => shifter(c, amount[index % amount.length])));
 };
+class caesar {
+    static encrypt(str, amount) {
+        return String.fromCharCode(...str.split("").map((c, index) => shifter(c, amount[index % amount.length])));
+    }
+    static decrypt(str, amount) {
+        return String.fromCharCode(...str.split("").map((c, index) => shifter(c, -amount[index % amount.length])));
+    }
+}
+
+
 
 
 let text = "alimert";
-let sifreli = caesarShift(text, [1, 4, 5, 3])
-let sifresiz = caesarShift(sifreli, [-1, -4, -5, -3])
+let sifreli = caesar.encrypt(text, [1, 2]);
+let sifresiz = caesar.decrypt(sifreli, [1, 2]);
 
 console.log(sifreli, sifresiz);
