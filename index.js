0412411d2163637792162a6088e9b32fd57338b0
Ali Mert <mail@alimert.net>
2020-06-14 13:26:30 +0300

    add new methods

--- index.js
+++ index.js
@@ -5,7 +5,17 @@ module.exports = class caesar {
     static encrypt(str, amount = [1, 9, 2, 3]) { //default password is 1923
         return String.fromCharCode(...str.split("").map((c, index) => this.shifter(c, amount[index % amount.length])));
     }
-    static decrypt(str, amount = [1, 9, 2, 3]) { //default password is 1923
+    static decrypt(str, amount = [1, 9, 2, 3]) {
         return String.fromCharCode(...str.split("").map((c, index) => this.shifter(c, -amount[index % amount.length])));
+    }
+
+
+    //* For directly encrypting from charcode, only single character for now
+
+    static encryptCharcode(char, amount) {
+        return this.shifter(char, amount);
+    }
+    static decryptCharcode(char, amount) {
+        return this.shifter(char, -amount);
     }
 }
